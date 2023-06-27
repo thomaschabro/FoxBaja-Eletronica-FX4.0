37f9f6c7e82e1fe39de2adfba4527e29d44d75ec
thomaschabro <81168920+thomaschabro@users.noreply.github.com>
2023-06-27 18:44:21 -0300

    adicionando para testar lcd

--- ECU-Painel/build/esp-idf/espressif__led_strip/cmake_install.cmake
+++ ECU-Painel/build/esp-idf/espressif__led_strip/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Espressif/frameworks/esp-idf-v5.0.2/BAJA/FoxBaja-Eletronica-FX4.0/ECU-Painel/managed_components/espressif__led_strip
+# Install script for directory: C:/Espressif/frameworks/esp-idf-v5.0.2/BAJA/FoxBaja-Eletronica-FX4.0/ECU-Painel/components/espressif__led_strip
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
--- ECU-Painel/main/main.c
+++ ECU-Painel/main/main.c
@@ -24,6 +24,9 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "driver/twai.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+#include "unistd.h"
 
 /* --------------------- Definitions and static variables ------------------ */
 //Example Configuration
@@ -74,6 +77,92 @@ static QueueHandle_t rx_task_queue;
 static SemaphoreHandle_t stop_ping_sem;
 static SemaphoreHandle_t ctrl_task_sem;
 static SemaphoreHandle_t done_sem;
+
+/* ------------------------------ Funções LCD ----------------------------- */
+static esp_err_t i2c_master_init(void)
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = GPIO_NUM_21,
+        .scl_io_num = GPIO_NUM_22,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000,
+    };
+
+    i2c_param_config(I2C_NUM_0, &conf);
+
+    return i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
+}
+
+void lcd_send_cmd (char cmd)
+{
+	esp_err_t err;
+	char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (cmd&0xf0);
+	data_l = ((cmd<<4)&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	data_t[2] = data_l|0x0C;  //en=1, rs=0
+	data_t[3] = data_l|0x08;  //en=0, rs=0
+
+	err= i2c_master_write_to_device (0x1, 0x0, data_t, 4, 1000);
+	// if (err != 0) ESP_LOGI(TAG, "Error no. %d in command", err);
+}
+
+void lcd_init (void)
+{
+	// 4 bit initialisation
+	usleep(50000);  // wait for >40ms
+	lcd_send_cmd (0x30);
+	usleep(4500);  // wait for >4.1ms
+	lcd_send_cmd (0x30);
+	usleep(200);  // wait for >100us
+	lcd_send_cmd (0x30);
+	usleep(200);
+	lcd_send_cmd (0x20);  // 4bit mode
+	usleep(200);
+
+  // dislay initialisation
+	lcd_send_cmd (0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters)
+	usleep(1000);
+	lcd_send_cmd (0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
+	usleep(1000);
+	lcd_send_cmd (0x01);  // clear display
+	usleep(1000);
+	usleep(1000);
+	lcd_send_cmd (0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
+	usleep(1000);
+	lcd_send_cmd (0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
+	usleep(2000);
+}
+
+void lcd_put_cur(int row, int col)
+{
+    switch (row)
+    {
+        case 0:
+            col |= 0x80;
+            break;
+        case 1:
+            col |= 0xC0;
+            break;
+    }
+
+    lcd_send_cmd (col);
+}
+
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_send_data (*str++);
+}
+
+void lcd_clear (void)
+{
+	lcd_send_cmd (0x01);
+	usleep(5000);
+}
 
 /* --------------------------- Tasks and Functions -------------------------- */
 
